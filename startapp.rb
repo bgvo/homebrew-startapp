0cc65fb84572772edc9a6c750e13559763a62d58
Borja GVO <borjavinuessa@gmail.com>
2023-06-19 12:38:27 +0200

    Remove SHA

--- startapp.rb
+++ startapp.rb
@@ -5,7 +5,7 @@ class Startapp < Formula
   desc "Create and start a new Rails app with a single command - best common gems and config right out of the box"
   homepage ""
   url "https://github.com/bgvo/createapp/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "072a52c1b5967c375ce20b73bada4735d9c50ee24759910028e6cbd9256b3b87"
+  sha256 ""
   license ""
 
   # depends_on "cmake" => :build
