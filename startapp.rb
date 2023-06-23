33d2acca517cf62c86bfee1467e98ca73f771573
Borja GVO <borjavinuessa@gmail.com>
2023-06-23 15:49:11 +0200

    Update startapp release

--- startapp.rb
+++ startapp.rb
@@ -4,11 +4,9 @@
 class Startapp < Formula
   desc "Create and start a new Rails app with a single command - best common gems and config right out of the box"
   homepage ""
-  url "https://github.com/bgvo/createapp/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/bgvo/startapp/archive/refs/tags/v1.1.0.tar.gz"
   sha256 ""
   license ""
-
-  # depends_on "cmake" => :build
 
   def install
     bin.install "startapp"
